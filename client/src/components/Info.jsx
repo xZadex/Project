5fdc0cea771442c137b748590162360897fb6cdd
Nicholas <codebyzade@gmail.com>
2022-11-15 22:48:37 -0500

    test info page

--- client/src/components/Info.jsx
+++ client/src/components/Info.jsx
@@ -1,15 +1,24 @@
 import React from 'react'
 import { useEffect } from 'react'
-import axios from 'axios'
+
 
 const Info = () => {
-    useEffect(()=>{
-        // axios call here
-    },[])
 
     return (
         <div className='container'>
-            <div>Hello</div>
+            <div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
         </div>
     )
 }
