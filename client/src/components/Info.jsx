046458a2b5f2d1d9ba5abffc6ce4842b1a6ba59a
kdang0 <73298064+kdang0@users.noreply.github.com>
2022-11-15 22:45:06 -0500

    modified data

--- client/src/components/Info.jsx
+++ client/src/components/Info.jsx
@@ -1,17 +1,25 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { useEffect } from 'react'
 import axios from 'axios'
 
 const Info = () => {
+    const [topgames, setTopGames] = useState(null);
+    const [allgames, setAllGames] = useState(null);
     useEffect(() => {
         // axios call here
         axios("http://localhost:8000/getTop100")
-            .then(res => console.log(res))
+            .then(res => setTopGames(res.data.response.ranks))
             .catch(err => console.log(err));
         axios("http://localhost:8000/getAllGames")
-            .then(res => console.log(res.data))
+            .then(res => setAllGames(res.data.applist.apps.app))
             .catch(err => console.error(err))
     }, [])
+
+    topgames?.map(object => object["name"] = allgames?.filter(obj=> {
+        return obj.appid === object.appid
+    }).map(obj => obj.name)[0])
+
+    console.log(topgames);
 
     return (
         <div className='container'>
