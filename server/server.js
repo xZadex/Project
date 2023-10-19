5fbf538eb36c8a327afe46eac81c285be52b12db
Nicholas Gibson <codebyzade@gmail.com>
2023-10-18 20:38:55 -0400

    testing cors configurations

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cors(
     {
-        origin: ["https://steame.vercel.app"],
+        origin: ["*"],
         methods:["POST", "GET"],
         credentials: true
     }
