759549309f8082f640e704cec62742eb0f56829c
Nicholas Gibson <codebyzade@gmail.com>
2023-10-18 21:16:19 -0400

    added another route to index.js to test

--- server/index.js
+++ server/index.js
@@ -17,6 +17,18 @@ app.get("/", (req, res) => {
     res.json("Hello again SteaME!");
 });
 
+app.get("/top100Games", (req, res) => {
+    const request = require('request');
+    const url = "https://api.steampowered.com/ISteamChartsService/GetGamesByConcurrentPlayers/v1/";
+    request(url, function(err, response, body){
+        if(!err && response.statusCode < 400){
+            // console.log(body);
+            res.header('Access-Control-Allow-Origin', 'https://steame.vercel.app');
+            res.send(body);
+        }
+    })
+});
+
 app.listen(3001, () => {
     console.log("Server is running on port 3001")
 })
